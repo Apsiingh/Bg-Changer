03b707fdecae3736d8a4e4ec1567ffb256f76fdc
Apsiingh <a.p.singh5508@gmail.com>
2024-03-20 20:14:56 +0530

    bg-changer

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ function App() {
   const [color, setcolor] = useState("black");
 
   return (
-   <div className='w-full h-screen duration-2000 rounded-xl'
+   <div className='w-full h-screen duration-2000 rounded-xl flex items-center justify-center'
     style={{backgroundColor:color}}
    >
+    <p class="text-3xl	 text-black-1000 ">Akhand Pratap Singh</p>
+
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>  
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-4 py-3 rounded-full'>
            <button
@@ -47,6 +49,13 @@ function App() {
             className='outline-none px-4 py-2 rounded-full text-white'
            style={{backgroundColor :"blue"}} 
            >Blue</button>
+           <button 
+             onClick={()=>{
+              setcolor("cyan")
+            }}
+            className='outline-none px-4 py-2 rounded-full text-white'
+           style={{backgroundColor :"cyan"}} 
+           >Cyan</button>
           
         </div>
       </div>
